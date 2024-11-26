19cd56b3b773d0880f99e8bbc85709228ce508ae
이민우 <mwtrain@naver.com>
2024-11-27 00:11:05 +0900

    충돌 처리 1차

--- SFMLProject1/Bullet.cpp
+++ SFMLProject1/Bullet.cpp
@@ -3,7 +3,7 @@
 
 Bullet::Bullet(const sf::Vector2f startPosition, const sf::Vector2f direction, float speed)
     : Projectile(startPosition, direction, speed) {
-    damage = 100.0f;
+    damage = 25.0f;
     shape.setRadius(5.0f);         // 발사체의 크기 설정
     shape.setFillColor(sf::Color::Magenta); // 발사체 색상 설정  
     shape.setPosition(position);
